fb3cb57511f0d6b700affc4952bb2b189e16ce19
JoshuaRocko <joshua.chirino@gmail.com>
2019-11-15 16:49:58 -0600

    ...

--- pizarraBack/web/scriptCanvas.js
+++ pizarraBack/web/scriptCanvas.js
@@ -45,7 +45,7 @@ btnDecreaseWidth.addEventListener('click', decreaseWidth);
 var dibuja = false;
 var color = 'blue';
 var lineWidth = 3;
-
+var lines = [];
 /* Funciones para incrementar o decrementar ancho de linea */
 
 function increaseWidth(event) {
@@ -86,56 +86,93 @@ function dibujaMouse(evento) {
       evento.layerX - this.offsetLeft,
       evento.layerY - this.offsetTop,
       context,
-      lineWidth
+      lineWidth,
+      true
     );
     x = evento.layerX - this.offsetLeft;
     y = evento.layerY - this.offsetTop;
   }
 }
 
-function dibujaLinea(color, x1, y1, x2, y2, lienzo, lineWidth) {
-  lienzo.beginPath();
-  lienzo.strokeStyle = color;
-  lienzo.lineWidth = lineWidth;
-  lienzo.moveTo(x1, y1);
-  lienzo.lineTo(x2, y2);
-  lienzo.stroke();
-  lienzo.closePath();
-}
-
-/* Guadar Archivo*/
-
-/* Obtenemos boton para guardar */
-const save = document.getElementById('save');
-save.addEventListener('click', saveCanvas);
-
-/* Funcion para convertir en JSON  */
-
-function saveCanvas() {
-  let canvasContent = canvas.toDataURL();
-  let data = { image: canvasContent };
-  let dataJson = JSON.stringify(data);
-  console.log(dataJson);
-}
-
-/* Load Archivo */
-const reader = new FileReader();
-console.log(reader);
-const load = document.getElementById('load');
-load.addEventListener('change', loadJson);
-
-function loadJson() {
-  if (this.files[0]) {
-    reader.readAsText(this.files[0]);
+function dibujaLinea(color, x1, y1, x2, y2, context, lineWidth, save) {
+  console.log(x1, y1, x2, y2);
+  context.beginPath();
+  context.strokeStyle = color;
+  context.lineWidth = lineWidth;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+  //guardamos las líneas que se dibujaron
+  if (save) {
+    lines.push([color, x1, y1, x2, y2, lineWidth]);
+  }
+  if (TogetherJS.running) {
+    TogetherJS.send({
+      type: 'draw',
+      color: color,
+      x1: x1,
+      y1: y1,
+      x2: x2,
+      y2: y2,
+      lineWidth: lineWidth
+    });
   }
 }
 
-reader.onload = function() {
-  let data = JSON.parse(reader.result);
-  let image = new Image();
-  image.onload = () => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(image, 0, 0);
-  };
-  image.src = data.image;
-};
+TogetherJS.hub.on('draw', function(msg) {
+  if (!msg.sameUrl) {
+    return;
+  }
+  draw(
+    msg.color,
+    msg.x1,
+    msg.y1,
+    msg.x2,
+    msg.y2,
+    context,
+    msg.lineWidth,
+    false
+  );
+});
+
+$('.user-color-pick').click(function() {
+  setColor(TogetherJS.require('peers').Self.color);
+  changeMouse();
+});
+
+// /* Guadar Archivo*/
+
+// /* Obtenemos boton para guardar */
+// const saveBtn = document.getElementById('save');
+// saveBtn.addEventListener('click', saveCanvas);
+
+// /* Funcion para convertir en JSON  */
+
+// function saveCanvas() {
+//   let canvasContent = canvas.toDataURL();
+//   let data = { image: canvasContent };
+//   let dataJson = JSON.stringify(data);
+//   console.log(dataJson);
+// }
+
+// /* Load Archivo */
+// const reader = new FileReader();
+// const load = document.getElementById('load');
+// load.addEventListener('change', loadJson);
+
+// function loadJson() {
+//   if (this.files[0]) {
+//     reader.readAsText(this.files[0]);
+//   }
+// }
+
+// reader.onload = function() {
+//   let data = JSON.parse(reader.result);
+//   let image = new Image();
+//   image.onload = () => {
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+//     context.drawImage(image, 0, 0);
+//   };
+//   image.src = data.image;
+// };
